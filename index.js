8e487c9cf1d36cd212960f00b3a3e8e7ff24c2cb
Wiztech-01 <lynnnyaoke@gmail.com>
2023-09-17 18:37:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-function addingEventListener() {
-}
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    const left = dodger.style.left;
+    let newLeft = parseInt(left) + 1;
+  
+    if (newLeft <= window.innerWidth) {
+      dodger.style.left = `${newLeft}px`;
+    }
+  }
